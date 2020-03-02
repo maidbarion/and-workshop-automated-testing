95ae2d8630bf18dbf95d54f0299acc14d06ce441
Adam Turner <adam.turner@and.digital>
2020-03-02 12:47:30 +0000

    Added husky hook for prepush to run unit tests. Broke a test to test hook

--- lab-2-improve-code/tests.test.js
+++ lab-2-improve-code/tests.test.js
@@ -1,33 +1,33 @@
-const greeting = require("./src/index");
+const greeting = require('./src/index');
 
-describe("Greeting Test", () => {
-  describe("Requirement 1", () => {
-    it("is a func", () => {
-      expect(typeof greeting).toEqual("function");
+describe('Greeting Test', () => {
+    describe('Requirement 1', () => {
+        it('is a func', () => {
+            expect(typeof greeting).toEqual('function');
+        });
     });
-  });
 
-  describe("Requirement 2", () => {
-    it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+    describe('Requirement 2', () => {
+        it("shows correct out put when in put is 'test'", () => {
+            expect(greeting('test')).not.toEqual('Hello, test.');
+        });
     });
-  });
 
-  describe("Requirement 3", () => {
-    it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+    describe('Requirement 3', () => {
+        it("shows correct out put when in put is 'test'", () => {
+            expect(greeting('test')).toEqual('Hello, test.');
+        });
     });
-  });
 
-  describe("Requirement 4", () => {
-    it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+    describe('Requirement 4', () => {
+        it("shows correct out put when in put is 'test'", () => {
+            expect(greeting('test')).toEqual('Hello, test.');
+        });
     });
-  });
 
-  describe("Requirement 5", () => {
-    it("shows correct out put when in put is 'test'", () => {
-      expect(greeting("test")).toEqual("Hello, test.");
+    describe('Requirement 5', () => {
+        it("shows correct out put when in put is 'test'", () => {
+            expect(greeting('test')).toEqual('Hello, test.');
+        });
     });
-  });
 });
