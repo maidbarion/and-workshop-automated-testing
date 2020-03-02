e1b2b02962a2ba1755c54034dd8bef23b1e9eefd
Adam Turner <adam.turner@and.digital>
2020-03-02 13:42:02 +0000

    Added missing unit tests

--- lab-2-improve-code/tests.test.js
+++ lab-2-improve-code/tests.test.js
@@ -2,32 +2,36 @@ const greeting = require('./src/index');
 
 describe('Greeting Test', () => {
     describe('Requirement 1', () => {
-        it('is a func', () => {
-            expect(typeof greeting).toEqual('function');
+        it("shows correct output when input is 'test'", () => {
+            expect(greeting('test')).toEqual('Hello, test.');
         });
     });
 
     describe('Requirement 2', () => {
-        it("shows correct out put when in put is 'test'", () => {
-            expect(greeting('test')).toEqual('Hello, test.');
+        it('shows correct output when input is null', () => {
+            expect(greeting()).toEqual('Hello, my friend.');
         });
     });
 
     describe('Requirement 3', () => {
-        it("shows correct out put when in put is 'test'", () => {
-            expect(greeting('test')).toEqual('Hello, test.');
+        it('shows correct output when input is JERRY', () => {
+            expect(greeting('JERRY')).toEqual('HELLO JERRY!');
         });
     });
 
     describe('Requirement 4', () => {
-        it("shows correct out put when in put is 'test'", () => {
-            expect(greeting('test')).toEqual('Hello, test.');
+        it("shows correct output when input is ['Jack', 'Jill']", () => {
+            expect(greeting(['Jack', 'Jill'])).toEqual(
+                'Hello, Jack, and Jill.'
+            );
         });
     });
 
     describe('Requirement 5', () => {
-        it("shows correct out put when in put is 'test'", () => {
-            expect(greeting('test')).toEqual('Hello, test.');
+        it("shows correct output when input is ['Jack', 'Jill', 'Jane']", () => {
+            expect(greeting(['Jack', 'Jill', 'Jane'])).toEqual(
+                'Hello, Jack, Jill, and Jane.'
+            );
         });
     });
 });
